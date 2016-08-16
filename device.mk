01d8c8c5325dc9e04d136d8e8bbac0ac155bdf1b
Patrick Tjin <pattjin@google.com>
2016-08-16 12:22:47 -0700

    caza: add nosdcard characteristic to device
    Bug: 30895189
    Change-Id: Ic2e040dc8d811ca84c910d42f6f7db35526802ff

--- device.mk
+++ device.mk
@@ -216,6 +216,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Product characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
