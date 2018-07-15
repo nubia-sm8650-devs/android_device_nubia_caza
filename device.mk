2609f350149ed38dda1189a7d2ec5b6065ab3f4a
Christian Oder <myself5@carbonrom.org>
2018-07-15 21:31:07 +0200

    caza: Add basic permission file for HotwordEnrollment
    Change-Id: I2986ba3599fe65a8da27649d98d70cd548257aa8

--- device.mk
+++ device.mk
@@ -106,6 +106,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
+# Hotword enrollment
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Partitions
 PRODUCT_BUILD_PVMFW_IMAGE := true
 
