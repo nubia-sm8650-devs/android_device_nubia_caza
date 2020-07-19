9d26cc04bf7b4fe21fcc87eebe029698d0b7ad1b
Demon000 <demonsingur@gmail.com>
2020-07-20 00:26:57 +0300

    caza: Build fastbootd
    Change-Id: I1056d3ed331c1590dce00f980bc78c6a5e23a799

--- device.mk
+++ device.mk
@@ -96,6 +96,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Partitions
 PRODUCT_BUILD_PVMFW_IMAGE := true
 
