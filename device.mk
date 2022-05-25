2c4ec9e6e8e18648beeb04472d402a2f93dc0b74
Albert Tang <tangalbert919@yahoo.com>
2022-05-25 11:37:59 -0500

    caza: Use OSS ST NFC
    Change-Id: I48765e548cd018a2420fe00994aecd48c4ca9227

--- device.mk
+++ device.mk
@@ -120,6 +120,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc-service.st
+
 # Partitions
 PRODUCT_BUILD_PVMFW_IMAGE := true
 
