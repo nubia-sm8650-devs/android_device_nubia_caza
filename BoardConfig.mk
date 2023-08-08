13048627f51c3b17fb2ca4c4cdafc411a1a497a8
Alexander Winkowski <dereference23@outlook.com>
2023-08-08 06:12:11 +0000

    caza: Add CPU flags
    Change-Id: Idd9d9689889a94016c93ac71dd2d44f277463231

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,3 +2,9 @@
 # SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv9-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a55
