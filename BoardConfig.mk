15ace94e5808b0a40d647ea25cc92183ce89eff8
Alexander Winkowski <dereference23@outlook.com>
2023-08-08 09:07:36 +0000

    caza: Add VINTF manifests
    Change-Id: I543f61a338134022cea1a2d51c444c6a51f42b8b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,3 +18,20 @@ TARGET_SCREEN_DENSITY := 480
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/device_framework_matrix.xml
+
+DEVICE_FRAMEWORK_MANIFEST_FILE := \
+    $(DEVICE_PATH)/framework_manifest.xml
+
+DEVICE_MANIFEST_FILE := \
+    $(DEVICE_PATH)/manifest.xml
+
+DEVICE_MATRIX_FILE := \
+    hardware/qcom-caf/common/compatibility_matrix.xml
+
+ODM_MANIFEST_FILES := \
+    $(DEVICE_PATH)/manifest_odm.xml
