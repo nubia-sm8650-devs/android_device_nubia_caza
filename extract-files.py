f5820e9b084fcc7abc388d3224c62bb5cde45d19
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:58:31 -0500

    caza: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

--- extract-files.py
+++ extract-files.py
@@ -9,9 +9,20 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 
+namespace_imports = [
+    'device/nubia/caza',
+    'hardware/qcom-caf/sm8650',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
 module = ExtractUtilsModule(
     'caza',
     'nubia',
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
