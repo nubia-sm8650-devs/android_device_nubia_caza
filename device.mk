cffede324fae69b50842c2ebfd7383f4f8652618
Yumi Yukimura <me.cafebabe@gmail.com>
2024-02-13 11:23:49 +0800

    caza: Build QTI Thermal HAL
    Change-Id: Iae471a16a0284cd24a598bac1a3ea0736faceb9f

--- device.mk
+++ device.mk
@@ -264,6 +264,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.qti
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
