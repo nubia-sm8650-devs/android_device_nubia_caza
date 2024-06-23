2aeb6e36e2fc6a40a890d71dcb4a0c6654079074
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    caza: Add RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495

--- device.mk
+++ device.mk
@@ -5,6 +5,9 @@
 
 DEVICE_PATH := device/nubia/caza
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
