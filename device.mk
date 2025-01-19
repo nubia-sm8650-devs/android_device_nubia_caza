86616ba30d796f8c353050eeebff2b223709b801
Alexander Winkowski <dereference23@outlook.com>
2025-01-19 08:37:04 +0000

    caza: Build RRO packages
    Change-Id: I64d080c3aa342b4d00c25d3a154b00ee2fe20ec9

--- device.mk
+++ device.mk
@@ -131,6 +131,16 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+PRODUCT_PACKAGES += \
+    CarrierConfigResCommon_Sys \
+    FrameworksResTarget_Vendor \
+    SystemUIResCommon_Sys \
+    TelephonyResCommon_Sys \
+    WifiResCommon_Sys \
+    WifiResOverlay \
+    WifiResTarget \
+    WifiResTarget_spf
+
 # Partitions
 PRODUCT_BUILD_PVMFW_IMAGE := true
 
