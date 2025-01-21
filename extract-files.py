09da80c37f7b4cac41754499d3f064140a1e3a47
Alexander Winkowski <dereference23@outlook.com>
2025-01-21 18:29:19 +0000

    patch libcamximageformatutils
    Change-Id: I33b315fe9f081330fcb2c8ae57753f37667abd12

--- extract-files.py
+++ extract-files.py
@@ -60,6 +60,8 @@ blob_fixups: blob_fixups_user_type = {
         .remove_needed('android.hidl.base@1.0.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
+    'vendor/lib64/libcamximageformatutils.so': blob_fixup()
+        .remove_needed('android.hardware.graphics.allocator-V1-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
