6655da7461aef31c55ba89b0bd5b0fc2122c04ea
Alexander Winkowski <dereference23@outlook.com>
2025-01-28 05:31:05 +0000

    sepolicy
    Change-Id: I5e5ed6d65998305911961bef073c75eb14636a99

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -191,7 +191,9 @@ BOOT_SECURITY_PATCH := 2025-03-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SELinux
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
