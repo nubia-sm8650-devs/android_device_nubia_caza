c6ffb713541f03a5734058010bd8f8b0781ce320
KisChang <734615869@qq.com>
2025-06-24 10:08:26 +0800

    Update device.mk add MindTheGapps

--- device.mk
+++ device.mk
@@ -380,5 +380,13 @@ PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
 
+#
+# MindTheGapps
+#
+WITH_GAPPS ?= false
+ifeq ($(WITH_GAPPS),true)
+  $(call inherit-product-if-exists,  vendor/gapps/arm64/arm64-vendor.mk )
+endif
+
 # Inherit from proprietary targets
 $(call inherit-product, vendor/nubia/caza/caza-vendor.mk)
