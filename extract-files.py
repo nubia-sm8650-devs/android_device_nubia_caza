14564928c5d11ff80eb38d83fec59b1e0be2b0ec
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-06 15:32:01 +0300

    cerro: commonize blobs

--- extract-files.py
+++ extract-files.py
@@ -9,7 +9,6 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -19,14 +18,10 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/nubia/cerro',
+    'vendor/nubia/sm8650-common',
     'hardware/qcom-caf/sm8650',
-    'hardware/qcom-caf/wlan',
-    'vendor/qcom/opensource/commonsys/display',
-    'vendor/qcom/opensource/commonsys-intf/display',
-    'vendor/qcom/opensource/dataservices',
-    'vendor/qcom/opensource/display',
 ]
+
 
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
@@ -34,47 +29,11 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
-        'com.qualcomm.qti.dpm.api@1.0',
         'libtensorflowlite_jni',
-        'vendor.qti.ImsRtpService-V1-ndk',
-        'vendor.qti.diaghal@1.0',
-        'vendor.qti.hardware.dpmaidlservice-V1-ndk',
-        'vendor.qti.hardware.dpmservice@1.0',
-        'vendor.qti.hardware.qccsyshal@1.0',
-        'vendor.qti.hardware.qccsyshal@1.1',
-        'vendor.qti.hardware.qccsyshal@1.2',
-        'vendor.qti.qesdhal@1.0',
-        'vendor.qti.qesdhal@1.1',
-        'vendor.qti.qesdhal@1.2',
-        'vendor.qti.qesdhal@1.3',
-        'vendor.qti.hardware.wifidisplaysession@1.0',
-        'vendor.qti.imsrtpservice@3.0',
-        'vendor.qti.imsrtpservice@3.1',
-        'vendor.qti.qccvndhal_aidl-V1-ndk',
-        'libskia',
     ): lib_fixup_vendor_suffix,
-    (
-        'libar-pal',
-        'libar-acdb',
-        'liblx-osal',
-        'libats',
-        'libagm',
-        'libpalclient',
-    ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'system_ext/bin/wfdservice64': blob_fixup()
-        .add_needed('libwfdservice_shim.so'),
-    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
-        .add_needed('libgui_shim.so'),
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .remove_needed('android.hidl.base@1.0.so')
-        .add_needed('libinput_shim.so'),
-    'system_ext/lib64/libwfdservice.so': blob_fixup()
-        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
-    'vendor/lib64/libcamximageformatutils.so': blob_fixup()
-        .remove_needed('android.hardware.graphics.allocator-V1-ndk.so'),
     (
         'vendor/lib64/camera/com.qti.ois.ois_dw9781_cerro_ov64b40.so',
         'vendor/lib64/camera/com.qti.ois.ois_dw9784_cerro_imx800.so',
@@ -84,6 +43,10 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/camera/com.zte.sensor.imx906_cerro.so',
         'vendor/lib64/camera/com.zte.sensor.ov50e40_cerro.so',
         'vendor/lib64/camera/com.zte.sensor.ov64b40_cerro.so',
+        'vendor/lib64/com.qti.feature2.fusion.so',
+        'vendor/lib64/hw/camera.qcom.sm8650.so',
+        'vendor/lib64/hw/camera.qcom.so',
+        'vendor/lib64/libchifeature2.so',
     ): blob_fixup().replace_needed(
         'android.hardware.graphics.allocator-V1-ndk.so',
         'android.hardware.graphics.allocator-V2-ndk.so',
